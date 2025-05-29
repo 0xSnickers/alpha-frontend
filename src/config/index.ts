3df9df6d1b00e68efa463b5582855a6ad727a3e3
0xSnickers <pakholee1997@gmail.com>
2025-05-29 20:53:07 +0800

    add code.

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import { bsc } from '@reown/appkit/networks'
 import type { AppKitNetwork } from '@reown/appkit/networks'
 
 // Get projectId from https://cloud.reown.com
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "" // this is a public projectId only to use on localhost
+export const projectId = "a49c238ba1b5c99754df1b17f5ec98c4" // this is a public projectId only to use on localhost
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
